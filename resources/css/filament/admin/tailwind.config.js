9076c714b632606312378b495f9d726e5ed92688
Lukmon Awoyemi <lukmon.awoyemi@softescu.com>
2026-09-03 20:19:04 +0100

    Filament theme: use unlayered CSS with !important to override utilities, fix content paths

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -3,8 +3,8 @@ import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
 export default {
     presets: [preset],
     content: [
-        './app/Filament/Admin/**/*.php',
-        './resources/views/filament/admin/**/*.blade.php',
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
 }
